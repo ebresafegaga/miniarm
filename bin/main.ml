ae07fd4e9c5ade64ce3ba80baa6fae8043d46874
Oghenevwogaga Ebresafe <gaga.ebresafe@tweag.io>
2023-04-07 22:38:34 +0100

    Give friendly error messages

--- bin/main.ml
+++ bin/main.ml
@@ -11,4 +11,23 @@ let () =
       regs
       |> List.iter (fun (reg, value) ->
              Printf.printf "%s = %d\n" (Ast.show_reg reg) value)
-  | Result.Error _ -> Printf.eprintf "An error occured"
+  | Result.Error e ->
+      Printf.eprintf
+        "Couldn't parse your program correctly. Did you enter an instruction \
+         that isn't supported yet?\n";
+
+      Printf.eprintf
+        "Also it might be helpful to remove blank lines from your program\n\n";
+
+      Printf.eprintf "This is the message I got from the parser: \n";
+
+      Printf.eprintf "%s\n" e
+  | exception e ->
+      let e = Printexc.to_string e in
+      Printf.eprintf
+        "The lexer couldn't properly tokenize your program. Did you use an \
+         instruction that is not yet supported?\n";
+
+      Printf.eprintf "This is the message I got: \n";
+
+      Printf.eprintf "%s" e
